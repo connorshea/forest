ce7f0f9486e136d330d5f04317130661116a5acf
Connor Shea <connor.james.shea@gmail.com>
2022-01-15 13:24:30 -0700

    Spawn a new lumberjack if we mawed the last one.

--- forest.rb
+++ forest.rb
@@ -66,33 +66,6 @@ class Forest
             end
           end
         elsif slottable.lumberjack?
-          # Lumberjacks each month will wander. They will move up to 3 times
-          # to a randomly picked spot that is adjacent in any direction. So
-          # for example a Lumberjack in the middle of your grid has 8 spots
-          # to move to. He will wander to a random spot. Then again. And
-          # finally for a third time.
-          #
-          # When the lumberjack moves if he encounters a Tree (not a sapling)
-          # he will stop and his wandering for that month comes to an end.
-          # He will then harvest the Tree for lumber. Remove the tree. Gain
-          # 1 piece of lumber. Lumberjacks will not harvest "Sapling". They
-          # will harvest an Elder Tree. Elder Trees are worth 2 pieces of
-          # lumber.
-          #
-          # Every 12 months the amount of lumber harvested is compared to the
-          # number of lumberjacks in the forest. A math formula
-          # is used to determine if we hire 1 or many lumberjacks. We hire a
-          # number of new lumberjacks based on lumber gathered. Let us say you
-          # have 10 lumberjacks. If you harvest 10-19 pieces of lumber you would
-          # hire 1 lumberjack. But if you harvest 20-29 pieces of lumber you
-          # would hire 2 lumberjacks. If you harvest 30-39 you would gain 3
-          # lumberjacks. And so forth.
-          #
-          # However if after a 12 month span the amount of lumber collected
-          # is below the number of lumberjacks then a lumberjack is let go
-          # to save money and 1 random lumberjack is removed from the forest.
-          # However you will never reduce your Lumberjack labor force below 0.
-
           # Skip this Lumberjack if it's already acted on this tick.
           next if slottable.acted_on_current_tick?
 
@@ -159,7 +132,11 @@ class Forest
               # Increment the Mawing Counter™.
               newly_mawed_lumberjacks += 1
 
-              # TODO: If the number of lumberjacks reaches 0, spawn a new one
+              # If we reach zero lumberjacks because the last one was mawed,
+              # spawn a new one somewhere.
+              if count_lumberjacks.zero?
+                populate_an_empty_grid_space(Lumberjack.new)
+              end
 
               stop_wandering = true
             end
